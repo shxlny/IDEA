bab62b61ae06eaf7d80c3c2711f6c0abe093d6e1
Yura1310 <103391854+Yura1310@users.noreply.github.com>
2025-01-22 21:09:46 +1000

    first achieve + nikita fix js

--- fefu/crowdapp/static/js/account.js
+++ fefu/crowdapp/static/js/account.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Проверка подключения обработчика
     console.log("Script loaded and button ready.");
-
     // Обработчик кнопки
     changeNicknameButton.addEventListener('click', function () {
         const newNickname = nicknameField.value.trim(); // Получаем значение никнейма
@@ -54,3 +53,26 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Получаем элементы
+    const publishButton = document.querySelector("publishButton") // Кнопка "Publish"
+    const modal = document.querySelector("popupModal"); // Модальное окно
+    const closeButton = document.querySelector("x-button"); // Кнопка "x" для закрытия модального окна
+
+    let isModalShown = false; // Флаг, который отслеживает, было ли модальное окно уже открыто
+
+  // Обработчик для кнопки "Publish"
+    publishButton.addEventListener("click", (event) => {
+        if (!isModalShown) {
+            event.preventDefault(); // Останавливаем стандартное поведение формы (чтобы модальное окно успело показаться)
+            modal.classList.remove("hidden"); // Показываем модальное окно
+            isModalShown = true; // Устанавливаем флаг, чтобы окно больше не открывалось
+        }
+    });
+  // Обработчик для кнопки закрытия "x"
+    closeButton.addEventListener("click", () => {
+        modal.classList.add("hidden"); // Скрываем модальное окно
+        });
+});
