c401f799b0a54afd1f3a0e46a3c4d65c4914f5e3
Yura1310 <103391854+Yura1310@users.noreply.github.com>
2025-01-24 04:06:55 +1000

    login + reg error

--- fefu/crowdapp/static/js/login.js
+++ fefu/crowdapp/static/js/login.js
@@ -16,3 +16,28 @@ function main(){
     const menu = document.getElementById("Mobile");
     menu.classList.toggle("hidden");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+        const passwordInput = document.getElementById("password");
+        const passwordError = document.getElementById("passwordError");
+        const continueButton = document.getElementById("ContinueButton")
+
+        console.log("Script loaded and button ready.");
+        // Обработчик ввода в поле пароля
+        passwordInput.addEventListener("input", function () {
+            if (passwordInput.value.length < 8) {
+                // Показываем сообщение об ошибке
+                passwordError.classList.remove("hidden");
+                passwordInput.classList.add("border-4")
+                passwordInput.style.borderColor = "red"; // Изменяем цвет границы
+                continueButton.classList.add("hidden")
+
+            } else {
+                // Скрываем сообщение об ошибке
+                passwordError.classList.add("hidden");
+                passwordInput.style.borderColor = "#B1C0E5"; // Возвращаем цвет границы
+                passwordInput.classList.remove("border-4")
+                continueButton.classList.remove("hidden")
+            }
+        });
+});
